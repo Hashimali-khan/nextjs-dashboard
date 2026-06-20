473a91e2b58c871afeaf57314c5e9d6aa074515b
Hashimali-khan <hashimalikhan2020@gmail.com>
2026-06-20 12:45:44 +0500

    fixed font error

--- app/ui/fonts.ts
+++ app/ui/fonts.ts
@@ -3,7 +3,7 @@ import { Lusitana } from 'next/font/google' ;
  
 
 export const inter = Inter({ subsets: ['latin'] });
-export const lusitana = Lusitana ({  subset :['latin'],
+export const lusitana = Lusitana ({  subsets :['latin'],
     weight: ['400', '700']}
 );
 
